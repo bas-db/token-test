2b572540a145b198e942c394cf2ff1ecc0731279
bas-db <76127152+bas-db@users.noreply.github.com>
2020-12-26 23:38:40 +0100

    Add TestToken and mint 10 million tokens on contract creation

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -1,1 +1,12 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+  // when contract is created, constructor is executed once
+  constructor() public ERC20("TestToken", "TT") {
+    // mints and sends initialSupply to contract creator / msg.sender 
+    _mint(msg.sender, 1e7 * 10 ** 18); // 10 million at 18 decimals
+  }
+}
