e5892a54451e26196b6afe0d86532385047d2b01
bas-db <76127152+bas-db@users.noreply.github.com>
2020-12-27 02:02:49 +0100

    Add hard cap of 100M tokens and improve mint function

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+pragma solidity ^0.6.6;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
 
-contract TestToken is ERC20 {
+contract TestToken is ERC20Capped {
   // when contract is created, constructor is executed once
-  constructor() public ERC20("TestToken", "TT") {
+  // hard cap at 100 million tokens at 18 decimals
+  constructor() public ERC20("TestToken", "TT") ERC20Capped(1e8 * (10 ** uint256(18))) {
+    _setupDecimals(18);
     // mints and sends initialSupply to contract creator / msg.sender 
-    _mint(msg.sender, 1e7 * 10 ** 18); // 10 million at 18 decimals
+    _mint(msg.sender, 1e7 * (10 ** uint256(decimals()))); // 10 million at 18 decimals
   }
 }
